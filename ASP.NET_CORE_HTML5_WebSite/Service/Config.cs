b629d0369422f89c69100751a54a2ebd271981c7
Ramin1001 <raminba@code.edu.az>
2021-05-09 00:31:07 +0400

    Add ConnnectionString, bind Config file with appsetting.json

---
+++ ASP.NET_CORE_HTML5_WebSite/Service/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_HTML5_WebSite.Service
+{
+    public class Config
+    {
+        public static string ConnectionString { get; set; }
+        public static string CompanyName { get; set; }
+        public static string CompanyPhone { get; set; }
+        public static string CompanyPhoneShort { get; set; }
+        public static string CompanyEmail { get; set; }
+
+
+    }
+}
---
+++ ASP.NET_CORE_HTML5_WebSite/Startup.cs
@@ -0,0 +1,51 @@
+using ASP.NET_CORE_HTML5_WebSite.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_HTML5_WebSite
+{
+    public class Startup
+    {
+
+        public IConfiguration Configuration { get; } // added manualy
+        public Startup(IConfiguration configuration) => Configuration = configuration; // added manualy
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // We bind the project property from appsettings.json to Config.cs to further manage it..
+            Configuration.Bind("Project", new Config()); // added manualy
+
+            services.AddControllersWithViews()
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}"); // added manualy
+            });
+        }
+    }
+}
