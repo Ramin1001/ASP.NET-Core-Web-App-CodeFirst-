5a72127fe80e580779988c56eb055fa711faf9ae
Ramin1001 <raminba@code.edu.az>
2021-05-14 00:01:30 +0400

    Add ServiceItem controll and view

---
+++ ASP.NET_CORE_HTML5_WebSite/Areas/Admin/Controllers/ServiceItemsController.cs
@@ -0,0 +1,70 @@
+﻿using ASP.NET_CORE_HTML5_WebSite.Domain;
+using ASP.NET_CORE_HTML5_WebSite.Domain.Entities;
+using ASP.NET_CORE_HTML5_WebSite.Service;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_HTML5_WebSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceItemsController : Controller
+    {
+
+        private readonly DataManager dataManager;
+        private readonly IWebHostEnvironment hostingEnvironment;
+
+        public ServiceItemsController(DataManager dataManager, IWebHostEnvironment hostingEnvironment)
+        {
+            this.dataManager = dataManager;
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            // If there not nothing, we create new. Otherwise we are take current element.
+            var entity = id == default ? new ServiceItem() : dataManager.ServiceItems.GetServiceItemById(id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ServiceItem model, IFormFile titleImageFile)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (titleImageFile != null)
+                {
+                    model.TitleImagePath = titleImageFile.FileName;
+                    using (var stream = new FileStream(Path.Combine(hostingEnvironment.WebRootPath, "images/", titleImageFile.FileName), FileMode.Create))
+                    {
+                        titleImageFile.CopyTo(stream);
+                    };
+                  
+                }
+                dataManager.ServiceItems.SaveServiceItem(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+
+            }
+
+
+            return View(model);
+        }
+
+
+        public IActionResult Delete(Guid id)
+        {
+            dataManager.ServiceItems.GetServiceItemById(id);
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+        }
+
+
+
+
+    }
+}
